7b09d20af693a711d0e054d7b2309c115f5d52b4
rishaaa <48066105+rishaaa@users.noreply.github.com>
2019-04-02 23:10:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!"
+        return "Hello!"
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
