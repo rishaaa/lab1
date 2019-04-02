da2e385b0b06e4d853e5c5521f1d2459bb5f9114
rishaaa <48066105+rishaaa@users.noreply.github.com>
2019-04-02 23:08:32 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='192.168.0.18', port=4000)
+
+@app.route('/test')
+def test():
+
+    POST_USERNAME = "python"
+    POST_PASSWORD = "python"
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        return "Object found"
+    else:
+        return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
